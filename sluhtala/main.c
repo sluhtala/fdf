9d6e5c1bda2dece119986c456e6fe56e7554ee78
Sasu Luhtala <sasu@sasu.local>
2020-02-20 01:35:04 +0200

    color works but if the angle is too high it only travels ones

--- sluhtala/main.c
+++ sluhtala/main.c
@@ -23,36 +23,40 @@ int		close_program(t_data *data)
 	return (0);
 }
 
-#include <stdio.h>
-int expand_lines(t_data *data, double num)
-{
-	static double x = 0;
-	int p1[2] = {data->width / 2, data->length / 2}; 
-	int p2[2] = {p1[0] * 2, p1[1]};
-	int dist = sqrt((data->width / 2) * (data->width / 2) + (data->length / 2) * (data->length / 2));
-	
-	x += num;
-	if (x >= 360)
-		x = 0;
-	if (x < 0)
-		x = 359;
-	//mlx_clear_window(data->mlx_ptr, data->mlx_win);
-	p2[0] = data->width / 2 + cos(x * M_PI / 180) * dist;
-	p2[1] = data->length / 2 - sin(x * M_PI / 180) * dist;
-	int col[2] = {0xffffff, 0xffffff};
-	draw_line(data, p1, p2, col);
-	return (0);
+t_vec3 **transformation(t_data *data)
+{	
+	data->pnt = transform_scale(*data, 30, 30, 46);
+	data->pnt = set_color(data);	
+	data->pnt = transform_rotate_z(*data, -30*(M_PI/180));
+	data->pnt = transform_rotate_x(*data, 30*(M_PI/180));
+	data->pnt = transform_move(*data, 20, 20, 0);
+	return (data->pnt);
 }
 
+#include <stdio.h>
+static t_vec3	**move(int key, t_data *data)
+{
+	mlx_clear_window(data->mlx_ptr, data->mlx_win);
+	if (key == up_key)
+		data->pnt = transform_move(*data, 0, -40, 0);
+	if (key == down_key)
+		data->pnt = transform_move(*data, 0, 40, 0);
+	if (key == right_key)
+		data->pnt = transform_move(*data, 40, 0, 0);
+	if (key == left_key)
+		data->pnt = transform_move(*data, -40, 0, 0);
+	draw_3d_grid(data, data->pnt, data->lenx, data->leny);
+	return (data->pnt);
+}
 int		input_manager(int key, t_data *data)
 {
 	printf("%d\n", key);
 	if (key == esc_key || key == 12)
 		close_program(data);
-	if (key == up_key)
-		expand_lines(data, -5);
-	if (key == down_key)
-		expand_lines(data, 5);
+	if (key == up_key || key == down_key || key == right_key || key == left_key)
+	{
+		data->pnt = move(key, data);
+	}
 	if (key == 18)
 	{
 		printf("parallel\n");
@@ -77,27 +81,6 @@ int		input_manager(int key, t_data *data)
 	return (key);
 }
 
-void print_map(t_vec3 **map, int lenx, int leny)
-{
-	int i;
-	int y;
-
-	i = 0;
-	y = 0;
-	while(y < leny)
-	{
-		i = 0;
-		while (i < lenx)
-		{
-			printf("x: %2f y:%2f z: %2f\n", map[y][i].x, map[y][i].y, map[y][i].z);
-			i++;
-		}
-		y++;
-		printf("\n");
-	}
-
-}
-
 int		main(int argc, char **argv)
 {
 	t_data d;
@@ -114,17 +97,11 @@ int		main(int argc, char **argv)
 	d.width = 1920 / 2;
 	d.length = 1080 /2 ;
 	d.pnt = file_manager(fd, &d.lenx, &d.leny);
-	data->pnt = transform_scale(d, 30/ 10, 30/10, 4);	
 	data->pnt = set_color(data);
-	printf("col.: %#x\n", data->pnt[0][0].color);
-	
+	data->pnt = transformation(data);	
 	//print_map(d.pnt, d.lenx, d.leny);
 	printf("col...: %#x\n", data->pnt[0][0].color);
-	data->pnt = transform_rotate_z(d, -30*(M_PI/180));
-	data->pnt = transform_rotate_x(d, 30*(M_PI/180));
-	data->pnt = transform_move(d, d.width/4, d.length/6, 0);
 	//print_map(d.pnt, d.lenx, d.leny);
-	//points = proj_ortho(points, map_size.x, map_size.y);
 	data->mlx_ptr = mlx_init();
 	if (!(data->mlx_win = mlx_new_window(data->mlx_ptr, data->width, data->length, "FDF")))
 		return (-1);
--- sluhtala/set_color.c
+++ sluhtala/set_color.c
@@ -2,28 +2,54 @@
 
 #include "fdf.h"
 #include <stdio.h>
-t_vec3	**set_color(t_data *data)
+
+static void get_minmax(t_data *data, int *min, int *max)
 {
-	int lenx;
-	int leny;
-	int col;
 	int i;
 	int j;
 
+	*min = 100000000;
+	*max = -100000000;
 	j = 0;
-	lenx = data->lenx;	
-	leny = data->leny;
-	while (j < leny)
+	while (j < data->leny)
 	{
 		i = 0;
-		while (i < lenx)
+		while (i < data->lenx)
 		{
-			data->pnt[j][i].color = 0xff - 0xff * (data->pnt[j][i].z / COLDIV);
-			//printf("i:%d j:%d col: %#x\n", i, j, data->pnt[j][i].color);
+			if (data->pnt[j][i].z > (double)*max)
+				*max = (int)data->pnt[j][i].z;
+			if (data->pnt[j][i].z < (double)*min)
+				*min = (int)data->pnt[j][i].z;
 			i++;
 		}
 		j++;
 	}
-	printf("col.: %#x\n", data->pnt[0][0].color);
+	if (*max == 0)
+		*max = 1;
+}
+
+t_vec3	**set_color(t_data *data)
+{
+	int max;
+	int min;
+	int col;
+	int i;
+	int j;
+
+	get_minmax(data, &min, &max);
+	j = 0;
+	while (j < data->leny)
+	{
+		i = 0;
+		while (i < data->lenx)
+		{
+			data->pnt[j][i].color = 0xff - 0xff * ((data->pnt[j][i].z - min) /
+			(max - min));
+			if (data->pnt[j][i].color > 255)
+				data->pnt[j][i].color = 255;
+			i++;
+		}
+		j++;
+	}
 	return (data->pnt);
 }
